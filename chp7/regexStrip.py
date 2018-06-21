6948552290a337a908fd9271c39be63405449771
joel <joelfadenrecht@gmail.com>
2018-06-20 22:53:13 -0700

    adding regexStrip.py

---
+++ chp7/regexStrip.py
@@ -0,0 +1,10 @@
+#!python3
+
+#this script will function the same as the string.strip() method but using regex.
+
+#TODO take input. check for second argument. 
+
+#TODO create regex. if nothing passed in second argument strip \s, else strip passed argument
+
+#TODO print/return output
+
